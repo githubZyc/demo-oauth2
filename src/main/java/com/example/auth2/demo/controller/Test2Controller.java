114cbdccb531cb1986801e374bd54ef9470b546f
githubZyc <289687858@qq.com>
2020-06-20 22:38:16 +0800

    Oauth2->新增po

--- src/main/java/com/example/auth2/demo/controller/Test2Controller.java
+++ src/main/java/com/example/auth2/demo/controller/Test2Controller.java
@@ -1,11 +1,17 @@
 package com.example.auth2.demo.controller;
 
+/**
+ * 功能描述: 测试2
+ * @Author ZYC
+ * @Date 2020/6/20 10:31 下午
+ * @Version 1.0
+ **/
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
-
+public class Test2Controller {
     /**
      * 功能描述:
      * @Author ZYC
@@ -14,8 +20,8 @@ public class TestController {
      * @Return java.lang.String
      * @Version 1.0
      **/
-    @RequestMapping("/test")
+    @RequestMapping("/test2")
     public String getString(){
-        return "hello mac auth2 demo";
+        return "hello mac auth2-test2 demo";
     }
 }
